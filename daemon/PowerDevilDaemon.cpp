4dfa51f4e1c08a3325ef02066034614f09f4c8cc
Dario Freddi <drf@kde.org>
2008-07-10 13:58:44 +0000

    Backporting code from KDE4Powersave
    svn path=/trunk/playground/utils/powerdevil/; revision=830444

--- daemon/PowerDevilDaemon.cpp
+++ daemon/PowerDevilDaemon.cpp
@@ -3,6 +3,11 @@
 #include <kdemacros.h>
 #include <KPluginFactory>
 #include <KPluginLoader>
+#include <KPassivePopup>
+#include <KIcon>
+#include <klocalizedstring.h>
+#include <kjob.h>
+#include <kworkspace/kdisplaymanager.h>
 
 #include "PowerDevilSettings.h"
 
@@ -16,12 +21,13 @@ K_EXPORT_PLUGIN(PowerDevilFactory("powerdevildaemon"))
 
 PowerDevilDaemon::PowerDevilDaemon(QObject *parent, const QList<QVariant>&)
  : KDEDModule(parent),
- m_notifier(Solid::Control::PowerManager::notifier())
+ m_notifier(Solid::Control::PowerManager::notifier()),
+ m_displayManager(new KDisplayManager())
 {
     /* First of all, let's check if a battery is present. If not, this
     module has to be shut down. */
 
-    Solid::DeviceNotifier *notifier = Solid::DeviceNotifier::instance();
+    //Solid::DeviceNotifier *notifier = Solid::DeviceNotifier::instance();
 	
     bool found = false;
     //get a list of all devices that are Batteries
@@ -47,19 +53,189 @@ PowerDevilDaemon::~PowerDevilDaemon()
 
 void PowerDevilDaemon::acAdapterStateChanged(int state)
 {
-    using namespace Solid::Control::PowerManager;
-    
-    if ( state == Plugged )
+    if ( state == Solid::Control::PowerManager::Plugged )
     {
-        setBrightness(PowerDevilSettings::aCBrightness());
-        setCpuFreqPolicy((CpuFreqPolicy) PowerDevilSettings::aCCpuPolicy());
+        Solid::Control::PowerManager::setBrightness(PowerDevilSettings::aCBrightness());
+        Solid::Control::PowerManager::setCpuFreqPolicy((Solid::Control::PowerManager::CpuFreqPolicy)
+	PowerDevilSettings::aCCpuPolicy());
     }
     
-    else if ( state == Unplugged )
+    else if ( state == Solid::Control::PowerManager::Unplugged )
     {
-        setBrightness(PowerDevilSettings::batBrightness());
-        setCpuFreqPolicy((CpuFreqPolicy) PowerDevilSettings::batCpuPolicy());
+        Solid::Control::PowerManager::setBrightness(PowerDevilSettings::batBrightness());
+        Solid::Control::PowerManager::setCpuFreqPolicy((Solid::Control::PowerManager::CpuFreqPolicy)
+	PowerDevilSettings::batCpuPolicy());
     }
+}
+
+void PowerDevilDaemon::batteryStateChanged(int state)
+{
+    switch(state)
+    {
+      //FIXME: Is this the right way?
+        case Solid::Control::PowerManager::Critical:
+            KPassivePopup::message(KPassivePopup::Boxed, "PowerDevil", i18n("Battery is at critical level"),
+                           KIcon("dialog-warning").pixmap(20,20), new QWidget(), 15000);
+            break;
+        case Solid::Control::PowerManager::Warning:
+            KPassivePopup::message(KPassivePopup::Boxed, "PowerDevil", i18n("Battery is at warning level"),
+                           KIcon("dialog-warning").pixmap(20,20), new QWidget(), 15000);
+            break;
+        case Solid::Control::PowerManager::Low:
+            KPassivePopup::message(KPassivePopup::Boxed, "PowerDevil", i18n("Battery is at low level"),
+                           KIcon("dialog-warning").pixmap(20,20), new QWidget(), 15000);
+            break;
+    }
+    
+    if(state == Solid::Control::PowerManager::Critical && 
+      Solid::Control::PowerManager::acAdapterState() == Solid::Control::PowerManager::Unplugged)
+    {
+        switch(PowerDevilSettings::batLowAction())
+        {
+            case Shutdown:
+                shutdown();
+                break;
+            case S2Disk:
+                Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::ToDisk);
+                break;
+            case S2Ram:
+                Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::ToRam);
+                break;
+            case Standby:
+                Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::Standby);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void PowerDevilDaemon::buttonPressed(int but)
+{
+    if(but == Solid::Control::PowerManager::LidClose)
+    {
+        if(Solid::Control::PowerManager::acAdapterState() == Solid::Control::PowerManager::Plugged)
+        {
+            switch(PowerDevilSettings::aCLidAction())
+            {
+                case Shutdown:
+                    shutdown();
+                    break;
+                case S2Disk:
+                    Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::ToDisk);
+                    break;
+                case S2Ram:
+                    Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::ToRam);
+                    break;
+                case Standby:
+                    Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::Standby);
+                    break;
+                case Lock:
+                    //lockScreen();
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            switch(PowerDevilSettings::batLidAction())
+            {
+                case Shutdown:
+                    shutdown();
+                    break;
+                case S2Disk:
+                    Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::ToDisk);
+                    break;
+                case S2Ram:
+                    Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::ToRam);
+                    break;
+                case Standby:
+                    Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::Standby);
+                    break;
+                case Lock:
+                    //lockScreen();
+                    break;
+                default:
+                    break;
+            }
+        }
+    } 
+}
+
+void PowerDevilDaemon::decreaseBrightness()
+{
+    int currentBrightness = Solid::Control::PowerManager::brightness() - 10;
+    
+    if(currentBrightness < 0)
+    {
+        currentBrightness = 0;
+    }
+    
+    //actionBrightnessSlider->setValue(currentBrightness);
+    Solid::Control::PowerManager::setBrightness(currentBrightness);
+}
+
+void PowerDevilDaemon::increaseBrightness()
+{
+    int currentBrightness = Solid::Control::PowerManager::brightness() + 10;
+    
+    if(currentBrightness > 100)
+    {
+        currentBrightness = 100;
+    }
+    
+    //actionBrightnessSlider->setValue(currentBrightness);
+    Solid::Control::PowerManager::setBrightness(currentBrightness);
+}
+
+
+void PowerDevilDaemon::shutdown()
+{
+    m_displayManager->shutdown(KWorkSpace::ShutdownTypeHalt, KWorkSpace::ShutdownModeTryNow);
+}
+
+void PowerDevilDaemon::suspendToDisk()
+{
+    /*if(configLockScreen)
+    {
+        lockScreen();
+    }*/
+    KJob * job = Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::ToDisk);
+    connect(job, SIGNAL(result(KJob *)), this, SLOT(suspendJobResult(KJob *)));
+    job->start();
+}
+
+void PowerDevilDaemon::suspendToRam()
+{
+    /*if(configLockScreen)
+    {
+        lockScreen();
+    }*/
+    KJob * job = Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::ToRam);
+    connect(job, SIGNAL(result(KJob *)), this, SLOT(suspendJobResult(KJob *)));
+    job->start();
+}
+
+void PowerDevilDaemon::standby()
+{
+    /*if(configLockScreen)
+    {
+        lockScreen();
+    }*/
+    KJob * job = Solid::Control::PowerManager::suspend(Solid::Control::PowerManager::Standby);
+    connect(job, SIGNAL(result(KJob *)), this, SLOT(suspendJobResult(KJob *)));
+    job->start();
+}
+
+void PowerDevilDaemon::suspendJobResult(KJob * job)
+{
+    if(job->error())
+    {
+        KPassivePopup::message(KPassivePopup::Boxed, i18n("WARNING"), job->errorString(),
+                           KIcon("dialog-warning").pixmap(20,20), dynamic_cast<QWidget *>(this), 15000);
+    }
+    //m_screenSaverIface->SimulateUserActivity(); //prevent infinite suspension loops
 }
 
 #include "PowerDevilDaemon.moc"
--- daemon/PowerDevilDaemon.h
+++ daemon/PowerDevilDaemon.h
@@ -2,8 +2,9 @@
 #define POWERDEVILDAEMON_H
 
 #include <kdedmodule.h>
-
 #include <solid/control/powermanager.h>
+
+class KDisplayManager;
 
 class PowerDevilDaemon : public KDEDModule
 {
@@ -15,10 +16,28 @@ public:
 
 private slots:
 	void acAdapterStateChanged(int state);
+	void batteryStateChanged(int state);
+	void decreaseBrightness();
+        void increaseBrightness();
+	void shutdown();
+        void suspendJobResult(KJob * job);
+        void suspendToDisk();
+        void suspendToRam();
+        void standby();
+	void buttonPressed(int but);
 
 private:
-	Solid::Control::PowerManager::Notifier * m_notifier;
+	enum IdleAction {
+            Shutdown,
+	    S2Disk, 
+	    S2Ram, 
+	    Standby, 
+	    Lock, 
+	    None
+        };
 	
+	Solid::Control::PowerManager::Notifier * m_notifier;
+	KDisplayManager * m_displayManager;
 };
 
 #endif /*POWERDEVILDAEMON_H*/
