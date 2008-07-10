34dc83addb86360d020705cc4b3bb29f4f466011
Dario Freddi <drf@kde.org>
2008-07-10 10:16:35 +0000

    Adding some configuration stuff
    svn path=/trunk/playground/utils/powerdevil/; revision=830394

--- daemon/PowerDevilDaemon.cpp
+++ daemon/PowerDevilDaemon.cpp
@@ -16,24 +16,52 @@ PowerDevilDaemon::PowerDevilDaemon(QObject *parent, const QList<QVariant>&)
  : KDEDModule(parent),
  m_notifier(Solid::Control::PowerManager::notifier())
 {
-  /* First of all, let's check if a battery is present. If not, this
-  module has to be shut down. */
+    /* First of all, let's check if a battery is present. If not, this
+    module has to be shut down. */
 
-  Solid::DeviceNotifier *notifier = Solid::DeviceNotifier::instance();
-      
-  bool found = false;
-  //get a list of all devices that are Batteries
-  foreach (Solid::Device device, Solid::Device::listFromType(Solid::DeviceInterface::Battery, QString()))
-    found = true;
-  
-  if ( !found )
-  {
-    //TODO: Shut the daemon down.
-  }
+    Solid::DeviceNotifier *notifier = Solid::DeviceNotifier::instance();
+	
+    bool found = false;
+    //get a list of all devices that are Batteries
+    foreach (Solid::Device device, Solid::Device::listFromType(Solid::DeviceInterface::Battery, QString()))
+      found = true;
+    
+    if ( !found )
+    {
+      //TODO: Shut the daemon down.
+    }
+
+    connect(m_notifier, SIGNAL(acAdapterStateChanged(int)), this, SLOT(acAdapterStateChanged(int)));
+    connect(m_notifier, SIGNAL(batteryStateChanged(int)), this, SLOT(batteryStateChanged(int)));
+    connect(m_notifier, SIGNAL(buttonPressed(int)), this, SLOT(buttonPressed(int)));
+    
+    //Setup initial state
+    acAdapterStateChanged(Solid::Control::PowerManager::acAdapterState());
 }
 
 PowerDevilDaemon::~PowerDevilDaemon()
 {
+}
+
+void Powersave::acAdapterStateChanged(int state)
+{
+    using namespace Solid::Control::PowerManager;
+    
+    if ( state == Plugged )
+    {
+        setBrightness(acBrightness);
+        setCpuFreqPolicy(acCpuPolicy);
+    }
+    
+    else if ( state == Unplugged )
+    {
+        onAc = false;
+        currentBrightness = batBrightness;
+        setBrightness(batBrightness);
+        setCpuFreqPolicy(batCpuPolicy);
+    }
+
+    actionBrightnessSlider->setValue(currentBrightness);
 }
 
 #include "PowerDevilDaemon.moc"
