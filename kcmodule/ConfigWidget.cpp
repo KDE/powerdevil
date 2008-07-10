2ae62f97ae4a3aa0dc74f53718db57de35e5b01c
Dario Freddi <drf@kde.org>
2008-07-10 17:21:00 +0000

    Adding i18n to KCModule
    svn path=/trunk/playground/utils/powerdevil/; revision=830515

--- kcmodule/ConfigWidget.cpp
+++ kcmodule/ConfigWidget.cpp
@@ -15,55 +15,55 @@ ConfigWidget::ConfigWidget(QWidget *parent)
 void ConfigWidget::fillUi()
 {
     
-    PoweredIdleCombo->addItem("Do nothing", 0);
-    PoweredIdleCombo->addItem("Shutdown", Shutdown);
-    BatteryCriticalCombo->addItem("Do nothing", 0);
-    BatteryCriticalCombo->addItem("Shutdown", Shutdown);
-    BatteryIdleCombo->addItem("Do nothing", 0);
-    BatteryIdleCombo->addItem("Shutdown", Shutdown);
+    PoweredIdleCombo->addItem(i18n("Do nothing"), 0);
+    PoweredIdleCombo->addItem(i18n("Shutdown"), Shutdown);
+    BatteryCriticalCombo->addItem(i18n("Do nothing"), 0);
+    BatteryCriticalCombo->addItem(i18n("Shutdown"), Shutdown);
+    BatteryIdleCombo->addItem(i18n("Do nothing"), 0);
+    BatteryIdleCombo->addItem(i18n("Shutdown"), Shutdown);
 
     Solid::Control::PowerManager::SuspendMethods methods = Solid::Control::PowerManager::supportedSuspendMethods();
 
     if(methods | Solid::Control::PowerManager::ToDisk)
     {
-        PoweredIdleCombo->addItem("Suspend to Disk", S2Disk);
-        BatteryCriticalCombo->addItem("Suspend to Disk", S2Disk);
-        BatteryIdleCombo->addItem("Suspend to Disk", S2Disk);
+        PoweredIdleCombo->addItem(i18n("Suspend to Disk"), S2Disk);
+        BatteryCriticalCombo->addItem(i18n("Suspend to Disk"), S2Disk);
+        BatteryIdleCombo->addItem(i18n("Suspend to Disk"), S2Disk);
     }
     if(methods | Solid::Control::PowerManager::ToRam)
     {
-        PoweredIdleCombo->addItem("Suspend to Ram", S2Ram);
-        BatteryCriticalCombo->addItem("Suspend to Ram", S2Ram);
-        BatteryIdleCombo->addItem("Suspend to Ram", S2Ram);
+        PoweredIdleCombo->addItem(i18n("Suspend to Ram"), S2Ram);
+        BatteryCriticalCombo->addItem(i18n("Suspend to Ram"), S2Ram);
+        BatteryIdleCombo->addItem(i18n("Suspend to Ram"), S2Ram);
     }
     if(methods | Solid::Control::PowerManager::Standby)
     {
-        PoweredIdleCombo->addItem("Standby", Standby);
-        BatteryCriticalCombo->addItem("Standby", Standby);
-        BatteryIdleCombo->addItem("Standby", Standby);
+        PoweredIdleCombo->addItem(i18n("Standby"), Standby);
+        BatteryCriticalCombo->addItem(i18n("Standby"), Standby);
+        BatteryIdleCombo->addItem(i18n("Standby"), Standby);
     }
 
     Solid::Control::PowerManager::CpuFreqPolicies policies = Solid::Control::PowerManager::supportedCpuFreqPolicies();
 
     if(policies | Solid::Control::PowerManager::Performance)
     {
-        PoweredFreqCombo->addItem("Performance", (int) Solid::Control::PowerManager::Performance);
-        BatteryFreqCombo->addItem("Performance", (int) Solid::Control::PowerManager::Performance);
+        PoweredFreqCombo->addItem(i18n("Performance"), (int) Solid::Control::PowerManager::Performance);
+        BatteryFreqCombo->addItem(i18n("Performance"), (int) Solid::Control::PowerManager::Performance);
     }
     if(policies | Solid::Control::PowerManager::OnDemand)
     {
-        PoweredFreqCombo->addItem("Dynamic (aggressive)", (int) Solid::Control::PowerManager::OnDemand);
-        BatteryFreqCombo->addItem("Dynamic (aggressive)", (int) Solid::Control::PowerManager::OnDemand);
+        PoweredFreqCombo->addItem(i18n("Dynamic (aggressive)"), (int) Solid::Control::PowerManager::OnDemand);
+        BatteryFreqCombo->addItem(i18n("Dynamic (aggressive)"), (int) Solid::Control::PowerManager::OnDemand);
     }
     if(policies | Solid::Control::PowerManager::Conservative)
     {
-        PoweredFreqCombo->addItem("Dynamic (less agressive)", (int) Solid::Control::PowerManager::Conservative);
-        BatteryFreqCombo->addItem("Dynamic (less agressive)", (int) Solid::Control::PowerManager::Conservative);
+        PoweredFreqCombo->addItem(i18n("Dynamic (less agressive)"), (int) Solid::Control::PowerManager::Conservative);
+        BatteryFreqCombo->addItem(i18n("Dynamic (less agressive)"), (int) Solid::Control::PowerManager::Conservative);
     }
     if(policies | Solid::Control::PowerManager::Powersave)
     {
-        PoweredFreqCombo->addItem("Powersave", (int) Solid::Control::PowerManager::Powersave);
-        BatteryFreqCombo->addItem("Powersave", (int) Solid::Control::PowerManager::Powersave);
+        PoweredFreqCombo->addItem(i18n("Powersave"), (int) Solid::Control::PowerManager::Powersave);
+        BatteryFreqCombo->addItem(i18n("Powersave"), (int) Solid::Control::PowerManager::Powersave);
     }
 
     // modified fields...
