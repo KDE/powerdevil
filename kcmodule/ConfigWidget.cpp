cba29f68a109aef74f70d56e1e5b51155fb1da2e
Dario Freddi <drf@kde.org>
2008-07-10 15:21:48 +0000

    Loading preferences into the widget
    svn path=/trunk/playground/utils/powerdevil/; revision=830474

--- kcmodule/ConfigWidget.cpp
+++ kcmodule/ConfigWidget.cpp
@@ -1,5 +1,7 @@
 #include "ConfigWidget.h"
 
+#include "PowerDevilSettings.h"
+
 #include <solid/control/powermanager.h>
 
 ConfigWidget::ConfigWidget(QWidget *parent)
@@ -67,7 +69,62 @@ void ConfigWidget::fillUi()
 
 void ConfigWidget::load()
 {
+    lockScreenOnResume->setChecked(PowerDevilSettings::configLockScreen());
+    dimDisplayOnIdle->setChecked(PowerDevilSettings::dimOnIdle());
+    PoweredBrightnessSlider->setValue(PowerDevilSettings::aCBrightness());
+    PoweredOffDisplayWhenIdle->setChecked(PowerDevilSettings::aCOffDisplayWhenIdle());
+    PoweredDisplayIdleTime->setValue(PowerDevilSettings::aCDisplayIdle());
+    PoweredIdleTime->setValue(PowerDevilSettings::aCIdle());
+    PoweredIdleCombo->setCurrentIndex(PoweredIdleCombo->findData(PowerDevilSettings::aCIdleAction()));
+    PoweredFreqCombo->setCurrentIndex(PoweredFreqCombo->findData(PowerDevilSettings::aCCpuPolicy()));
+    BatteryBrightnessSlider->setValue(PowerDevilSettings::batBrightness());
+    BatteryOffDisplayWhenIdle->setChecked(PowerDevilSettings::batOffDisplayWhenIdle());
+    BatteryDisplayIdleTime->setValue(PowerDevilSettings::batDisplayIdle());
+    BatteryIdleTime->setValue(PowerDevilSettings::batIdle());
+    BatteryIdleCombo->setCurrentIndex(BatteryIdleCombo->findData(PowerDevilSettings::batIdleAction()));
+    BatteryCriticalCombo->setCurrentIndex(BatteryCriticalCombo->findData(PowerDevilSettings::batLowAction()));
+    BatteryFreqCombo->setCurrentIndex(BatteryFreqCombo->findData(PowerDevilSettings::batCpuPolicy()));
 
+    switch(PowerDevilSettings::batLidAction())
+    {
+        case None:
+            laptopClosedBatteryNone->setChecked(true);
+            break;
+        case S2Disk:
+            laptopClosedBatteryHibernate->setChecked(true);
+            break;
+        case S2Ram:
+            laptopClosedBatterySuspend->setChecked(true);
+            break;
+        case Shutdown:
+            laptopClosedBatteryShutdown->setChecked(true);
+            break;
+        case Lock:
+            laptopClosedBatteryBlank->setChecked(true);
+            break;
+        default:
+            break;
+    }
+    switch(PowerDevilSettings::aCLidAction())
+    {
+        case None:
+            laptopClosedACNone->setChecked(true);
+            break;
+        case S2Disk:
+            laptopClosedACHibernate->setChecked(true);
+            break;
+        case S2Ram:
+            laptopClosedACSuspend->setChecked(true);
+            break;
+        case Shutdown:
+            laptopClosedACShutdown->setChecked(true);
+            break;
+        case Lock:
+            laptopClosedACBlank->setChecked(true);
+            break;
+        default:
+            break;
+    }
 }
 
 void ConfigWidget::save()
