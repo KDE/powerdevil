f36cfb8a88c2c8cd32041a23c58bda62a00ed28e
Dario Freddi <drf@kde.org>
2008-07-11 10:53:04 +0000

    Removing all Krazy issues
    svn path=/trunk/playground/utils/powerdevil/; revision=830873

--- daemon/PowerDevilDaemon.cpp
+++ daemon/PowerDevilDaemon.cpp
@@ -1,3 +1,26 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Dario Freddi <drf@kdemod.ath.cx>                *
+ *   Copyright (C) 2007-2008 by Riccardo Iaconelli <riccardo@kde.org>      *
+ *   Copyright (C) 2007-2008 by Sebastian Kuegler <sebas@kde.org>          *
+ *   Copyright (C) 2007 by Luka Renko <lure@kubuntu.org>                   *
+ *   Copyright (C) 2008 by Thomas Gillespie <tomjamesgillespie@gmail.com>  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
 #include "PowerDevilDaemon.h"
 
 #include <kdemacros.h>
@@ -253,7 +276,7 @@ void PowerDevilDaemon::suspendJobResult(KJob * job)
 {
     if(job->error())
     {
-        KPassivePopup::message(KPassivePopup::Boxed, i18n("WARNING"), job->errorString(),
+        KPassivePopup::message(KPassivePopup::Boxed, i18n("PowerDevil"), job->errorString(),
                            KIcon("dialog-warning").pixmap(20,20), dynamic_cast<QWidget *>(this), 15000);
     }
     m_screenSaverIface->SimulateUserActivity(); //prevent infinite suspension loops
