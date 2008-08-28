dce2af417bc8bd7ce8d0a02bdc3380dc8ede8822
Chusslove Illich <caslav.ilic@gmx.net>
2008-08-28 09:33:29 +0000

    Do not crash on literal *.h pattern; extract all messages.
    svn path=/trunk/playground/utils/powerdevil/; revision=853779

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
-powerdevil_subdirs="kcmodule daemon plasma/runner plasma/applet"
-$EXTRACTRC `find $powerdevil_subdirs -name \*.ui` >> rc.cpp || exit 11
-$EXTRACTRC `find $powerdevil_subdirs -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find $powerdevil_subdirs -name \*.cpp -o -name \*.h` *.cpp *.h -o $podir/powerdevil.pot
+$EXTRACTRC `find -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp || exit 11
+$XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/powerdevil.pot
 rm -f rc.cpp
 
