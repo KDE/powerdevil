7600724cf555acbe6390ec627d19e464c410d717
Michael Jansen <kde@michael-jansen.biz>
2008-10-13 00:38:18 +0000

    Add Q_CLASSINFO . KDEDModules have to have one because they are registered with dbus from kded.
    svn path=/trunk/KDE/kdebase/workspace/; revision=870665

--- daemon/PowerDevilDaemon.h
+++ daemon/PowerDevilDaemon.h
@@ -38,6 +38,7 @@ class OrgKdeScreensaverInterface;
 class KDE_EXPORT PowerDevilDaemon : public KDEDModule
 {
     Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.PowerDevil")
 
 public:
     PowerDevilDaemon(QObject *parent, const QList<QVariant>&);
