28730c6cc3d1933033b0e6d11f766e472ffcd854
Dario Freddi <drf@kde.org>
2009-02-08 12:54:08 +0000

    BUG: 183273
    Backporting fix to 4.2
    
    
    svn path=/branches/KDE/4.2/kdebase/workspace/; revision=923147

--- daemon/PowerDevilDaemon.h
+++ daemon/PowerDevilDaemon.h
@@ -117,6 +117,8 @@ private Q_SLOTS:
 
     void batteryRemainingTimeChanged(int time);
 
+    void loggingOut(int confirm, int sdtype, int sdmode);
+
 Q_SIGNALS:
     void lidClosed(int code, const QString &action);
     void errorTriggered(const QString &error);
