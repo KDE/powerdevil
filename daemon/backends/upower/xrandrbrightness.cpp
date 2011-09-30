ee38a4e9a41ce9337c6707302158a8ab941230e3
Dario Freddi <drf@kde.org>
2011-09-30 22:22:15 +0200

    Exclude XRandR 1.3 specific bits from compilation if XRandR 1.3 is not found
    BUG: 271526

--- daemon/backends/upower/xrandrbrightness.cpp
+++ daemon/backends/upower/xrandrbrightness.cpp
@@ -19,6 +19,8 @@
 
 #include "xrandrbrightness.h"
 
+#include <config-X11.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -53,10 +55,14 @@ XRandrBrightness::XRandrBrightness()
         return;
     }
 
-    if (minor > 2)
+#ifdef HAS_RANDR_1_3
+    if (minor > 2) {
         m_resources = XRRGetScreenResourcesCurrent(QX11Info::display(), QX11Info::appRootWindow()); // version 1.3, faster version
-    else
+    } else
+#endif
+    {
         m_resources = XRRGetScreenResources(QX11Info::display(), QX11Info::appRootWindow());
+    }
 
     if (!m_resources)
     {
