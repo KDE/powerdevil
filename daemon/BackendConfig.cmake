4a6c8286cea44aa5affdfdd7bb99087a92788665
Bhushan Shah <bhush94@gmail.com>
2016-06-02 11:20:05 +0530

    Make udev seriously mandatory
    Summary:
    73e13daef13 done just half work of ripping of HAVE_UDEV in c++ code,
    this however didn't remove the UDEV_FOUND ifdefs from the cmake code, so
    this UDEV_FOUND ifs in cmake code makes very less sense.
    
    Test Plan: builds
    
    Reviewers: broulik, #plasma
    
    Reviewed By: broulik, #plasma
    
    Subscribers: graesslin, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D1742

--- daemon/BackendConfig.cmake
+++ daemon/BackendConfig.cmake
@@ -3,48 +3,46 @@
 
 
 ########################## UPower Backend #####################################
-if (UDEV_FOUND)
-    include_directories(${CMAKE_CURRENT_SOURCE_DIR}/backends/upower
-                        ${X11_INCLUDE_DIR}
-                        ${X11_Xrandr_INCLUDE_PATH})
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/backends/upower
+                    ${X11_INCLUDE_DIR}
+                    ${X11_Xrandr_INCLUDE_PATH})
 
-    set(powerdevilupowerbackend_SRCS
-        ${PowerDevil_SOURCE_DIR}/daemon/powerdevil_debug.cpp
-        backends/upower/upowersuspendjob.cpp
-        backends/upower/login1suspendjob.cpp
-        backends/upower/powerdevilupowerbackend.cpp
-        backends/upower/xrandrbrightness.cpp
-        backends/upower/xrandrxcbhelper.cpp
-        backends/upower/udevqtclient.cpp
-        backends/upower/udevqtdevice.cpp
-    )
+set(powerdevilupowerbackend_SRCS
+    ${PowerDevil_SOURCE_DIR}/daemon/powerdevil_debug.cpp
+    backends/upower/upowersuspendjob.cpp
+    backends/upower/login1suspendjob.cpp
+    backends/upower/powerdevilupowerbackend.cpp
+    backends/upower/xrandrbrightness.cpp
+    backends/upower/xrandrxcbhelper.cpp
+    backends/upower/udevqtclient.cpp
+    backends/upower/udevqtdevice.cpp
+)
 
-    set_source_files_properties(
-    ${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.xml
-    ${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.Device.xml
-    PROPERTIES NO_NAMESPACE TRUE)
+set_source_files_properties(
+${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.xml
+${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.Device.xml
+PROPERTIES NO_NAMESPACE TRUE)
 
-    qt5_add_dbus_interface(powerdevilupowerbackend_SRCS
-    ${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.xml
-    upower_interface)
+qt5_add_dbus_interface(powerdevilupowerbackend_SRCS
+${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.xml
+upower_interface)
 
-    qt5_add_dbus_interface(powerdevilupowerbackend_SRCS
-    ${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.Device.xml
-    upower_device_interface)
+qt5_add_dbus_interface(powerdevilupowerbackend_SRCS
+${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.Device.xml
+upower_device_interface)
 
-    qt5_add_dbus_interface(powerdevilupowerbackend_SRCS
-    ${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.KbdBacklight.xml
-    upower_kbdbacklight_interface)
+qt5_add_dbus_interface(powerdevilupowerbackend_SRCS
+${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/dbus/org.freedesktop.UPower.KbdBacklight.xml
+upower_kbdbacklight_interface)
 
-    set(powerdevilupowerbackend_LIBS ${X11_LIBRARIES} Qt5::Widgets KF5::Auth ${X11_Xrandr_LIB} ${UDEV_LIBS} ${XCB_XCB_LIBRARY}
-    ${XCB_RANDR_LIBRARY})
-    ## backlight helper executable
-    add_executable(backlighthelper backends/upower/backlighthelper.cpp ${PowerDevil_SOURCE_DIR}/daemon/powerdevil_debug.cpp ${backlighthelper_mocs})
-    target_link_libraries(backlighthelper Qt5::Core KF5::Auth KF5::I18n)
-    install(TARGETS backlighthelper DESTINATION ${KAUTH_HELPER_INSTALL_DIR})
-    kauth_install_helper_files(backlighthelper org.kde.powerdevil.backlighthelper root)
-    kauth_install_actions(org.kde.powerdevil.backlighthelper ${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/backlight_helper_actions.actions)
-endif ()
+set(powerdevilupowerbackend_LIBS ${X11_LIBRARIES} Qt5::Widgets KF5::Auth ${X11_Xrandr_LIB} ${UDEV_LIBS} ${XCB_XCB_LIBRARY}
+${XCB_RANDR_LIBRARY})
+## backlight helper executable
+add_executable(backlighthelper backends/upower/backlighthelper.cpp ${PowerDevil_SOURCE_DIR}/daemon/powerdevil_debug.cpp ${backlighthelper_mocs})
+target_link_libraries(backlighthelper Qt5::Core KF5::Auth KF5::I18n)
+install(TARGETS backlighthelper DESTINATION ${KAUTH_HELPER_INSTALL_DIR})
+kauth_install_helper_files(backlighthelper org.kde.powerdevil.backlighthelper root)
+kauth_install_actions(org.kde.powerdevil.backlighthelper ${CMAKE_CURRENT_SOURCE_DIR}/backends/upower/backlight_helper_actions.actions)
 
 ########################## Daemon variables ################################
 
