f138ef98fc9e78545a1df3e8646aadcbdda7105e
Kai Uwe Broulik <kde@privat.broulik.de>
2020-09-23 17:25:00 +0000

    Show "you might need to reconnect charger" hint only when charging/fully charged
    The hint shifts the UI about so don't show it we aren't actually charging right now
    
    Also move code into a separate method for cleanliness
    
    
    (cherry picked from commit aa6dba846f0e0a8bb049a794f11a2b3fa953db45)

--- kcmodule/global/GeneralPage.h
+++ kcmodule/global/GeneralPage.h
@@ -42,6 +42,7 @@ private Q_SLOTS:
     void configureNotifications();
     void onServiceRegistered(const QString &service);
     void onServiceUnregistered(const QString &service);
+    void onChargeStopThresholdChanged(int threshold);
 
 private:
     void setChargeThresholdSupported(bool supported);
