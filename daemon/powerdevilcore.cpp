acaa7fb52ce0a326949f8eedb2e7611f039da266
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2023-04-20 13:02:53 +0200

    Delay Solid::Battery instatiation until actually required
    The Device is only required when showing the notification, so do the
    trivial checks based on currentPercent/previousPercent first.
    
    (cherry picked from commit 5472967165fb780cd4780a6f32d79106cb7222d9)

--- daemon/powerdevilcore.cpp
+++ daemon/powerdevilcore.cpp
@@ -550,6 +550,12 @@ void Core::emitRichNotification(const QString &evid, const QString &title, const
 bool Core::emitBatteryChargePercentNotification(int currentPercent, int previousPercent, const QString &udi, Core::ChargeNotificationFlags flags)
 {
     if (m_peripheralBatteriesPercent.contains(udi)) {
+        // Show the notification just once on each normal->low transition
+        if (currentPercent > PowerDevilSettings::peripheralBatteryLowLevel() ||
+            previousPercent <= PowerDevilSettings::peripheralBatteryLowLevel()) {
+            return false;
+        }
+
         using namespace Solid;
         Device device(udi);
         Battery *b = qobject_cast<Battery *>(device.asDeviceInterface(DeviceInterface::Battery));
@@ -568,9 +574,7 @@ bool Core::emitBatteryChargePercentNotification(int currentPercent, int previous
             return false;
         }
 
-        if (currentPercent <= PowerDevilSettings::peripheralBatteryLowLevel() &&
-            previousPercent > PowerDevilSettings::peripheralBatteryLowLevel()) {
-
+        {
             QString name = device.product();
             if (!device.vendor().isEmpty()) {
                 name = i18nc("%1 is vendor name, %2 is product name", "%1 %2", device.vendor(), device.product());
@@ -603,11 +607,9 @@ bool Core::emitBatteryChargePercentNotification(int currentPercent, int previous
             }
 
             emitNotification(QStringLiteral("lowperipheralbattery"), title, msg, icon);
-
-            return true;
         }
 
-        return false;
+        return true;
     }
 
     // Make sure a notificaton that's kept open updates its percentage live.
