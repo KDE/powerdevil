KDE/powerdevil

commit e9c984a78c9ace207f98922d018ea3ef58ea7cd3

actions/bundled/dpms: Less fragile code for pre-turn-off fading
Simplifies the code for better readability.

Also solves an issue on X11 when an activating screen locker
reduces the idle timeout, or for idle timeouts shorter than the
hardcoded 5 second fade-out grace period. In either of these cases,
both the fade and action timeouts would be signaled immediately
after one another, but in the wrong order. The observed effect was
that the screen turned off first, the user moves their mouse to
avoid locking, and the screen comes back on but fades out anyway.

The refactored version avoids these kinds of race conditions by
simply connecting the turn-off action to the screen helper's fadedOut
signal, or in case of Wayland, directly to the timeout like before.
No hardcoded fade-out times are necessary anymore.

daemon/actions/bundled/dpms.cpp
@@ -33,7 +33,6 @@
 K_PLUGIN_CLASS_WITH_JSON(PowerDevil::BundledActions::DPMS, "powerdevildpmsaction.json")
 
 using namespace std::chrono_literals;
-static constexpr auto s_fadeTime = 5s;
 
 namespace PowerDevil
 {
@@ -50,6 +49,10 @@ DPMS::DPMS(QObject *parent)
         auto fadeEffect = new PowerDevil::KWinKScreenHelperEffect(this);
         connect(this, &DPMS::startFade, fadeEffect, &PowerDevil::KWinKScreenHelperEffect::start);
         connect(this, &DPMS::stopFade, fadeEffect, &PowerDevil::KWinKScreenHelperEffect::stop);
+        connect(fadeEffect, &PowerDevil::KWinKScreenHelperEffect::fadedOut, this, &DPMS::turnOffOnIdleTimeout);
+    } else {
+        // short-cut without fadeEffect in between timeout and actual action (compositor handles fading)
+        connect(this, &DPMS::startFade, this, &DPMS::turnOffOnIdleTimeout);
     }
 
     // Listen to the policy agent
@@ -94,39 +97,33 @@ bool DPMS::isSupported()
 
 void DPMS::onWakeupFromIdle()
 {
-    if (isSupported()) {
-        Q_EMIT stopFade();
-    }
+    Q_EMIT stopFade(); // only actively used in X11
+
     if (m_oldKeyboardBrightness > 0) {
         setKeyboardBrightnessHelper(m_oldKeyboardBrightness);
         m_oldKeyboardBrightness = 0;
     }
 }
 
-void DPMS::onIdleTimeout(std::chrono::milliseconds timeout)
+void DPMS::onIdleTimeout(std::chrono::milliseconds /*timeout*/)
+{
+    Q_EMIT startFade();
+}
+
+void DPMS::turnOffOnIdleTimeout()
 {
     // Do not inhibit anything even if idleTimeout reaches because we are inhibit
     if (m_inhibitScreen) {
         return;
     }
 
-    if (timeout == m_idleTime - s_fadeTime
-        || (PowerDevil::PolicyAgent::instance()->screenLockerActive() && timeout == m_idleTimeoutWhenLocked - s_fadeTime)) { // fade out screen
-
-        if (isSupported()) {
-            // only used in X11
-            Q_EMIT startFade();
-        }
-
-    } else if (timeout == m_idleTime || (PowerDevil::PolicyAgent::instance()->screenLockerActive() && timeout == m_idleTimeoutWhenLocked)) {
-        const int keyboardBrightness = backend()->keyboardBrightness();
-        if (keyboardBrightness > 0) {
-            m_oldKeyboardBrightness = keyboardBrightness;
-            setKeyboardBrightnessHelper(0);
-        }
-        if (isSupported()) {
-            m_dpms->switchMode(KScreen::Dpms::Off);
-        }
+    const int keyboardBrightness = backend()->keyboardBrightness();
+    if (keyboardBrightness > 0) {
+        m_oldKeyboardBrightness = keyboardBrightness;
+        setKeyboardBrightnessHelper(0);
+    }
+    if (isSupported()) {
+        m_dpms->switchMode(KScreen::Dpms::Off);
     }
 }
 
@@ -190,7 +187,6 @@ void PowerDevil::BundledActions::DPMS::registerDpmsOffOnIdleTimeout(std::chrono:
 {
     if (timeout > 0s) {
         registerIdleTimeout(timeout);
-        registerIdleTimeout(timeout - s_fadeTime); // start screen fade a bit earlier to alert user
     }
 }
 

daemon/actions/bundled/dpms.h
@@ -44,6 +44,7 @@ public:
     bool loadAction(const PowerDevil::ProfileSettings &profileSettings) override;
 
 private Q_SLOTS:
+    void turnOffOnIdleTimeout();
     void onUnavailablePoliciesChanged(PowerDevil::PolicyAgent::RequiredPolicies policies);
     void onScreenLockerActiveChanged(bool active);
 

commit 8effa40735b4d98f0554f97b41f103e7e8f9c65c

Check if idle inhibitor is active before blanking the screen
This was accidently removed during refactor of this area.

This fixes regression introduced in e9c984a78c9ace207f98922d018ea3ef58ea7cd3

BUG: 482141

daemon/actions/bundled/dpms.cpp
@@ -116,16 +116,14 @@ void DPMS::onWakeupFromIdle()
 
 void DPMS::onIdleTimeout(std::chrono::milliseconds /*timeout*/)
 {
-    Q_EMIT startFade();
+    // Do not inhibit anything even if idleTimeout reaches because we are inhibit
+    if (!m_inhibitScreen && isSupported()) {
+        Q_EMIT startFade();
+    }
 }
 
 void DPMS::turnOffOnIdleTimeout()
 {
-    // Do not inhibit anything even if idleTimeout reaches because we are inhibit
-    if (m_inhibitScreen) {
-        return;
-    }
-
     const int keyboardBrightness = core()->keyboardBrightnessController()->keyboardBrightness();
     if (keyboardBrightness > 0) {
         m_oldKeyboardBrightness = keyboardBrightness;