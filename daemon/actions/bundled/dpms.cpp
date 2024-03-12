KDE/powerdevil

commit c58085b4ca3cf5456dd7cdfa4189d06b38a3e488

actions/bundled/dpms: Handle very short timeout-when-locked better
BUG: 476962

For automatic display turn-off, we now allow setting an idle timeout
when the screen is locked, down to 0 seconds. This makes sense when
the session transitions from unlocked to locked, allowing the user to
turn off the display at the same time as the lockscreen would appear.

Unfortunately, it doesn't work for a 0 second timeout. The issue is
that low timeout values also affect user interaction on wakeup,
i.e. when trying to enter the password the user now has 0 seconds
available to type their password. This prevents us from simply
removing the check for >0 seconds when registering DPMS timeouts.

Instead, this commit splits the timeout when locked into two internal
member variables.

For the traditional "timeout when locked" value, we now apply a
(hardcoded) minimum duration to allow uninterrupted authentication
when the lock screen is already active.

In addition, we introduce a new "timeout when activating lock" value
that uses the configured value as is (more or less - applying an
extremely short minimum may help prevent unintended behavior).
This new timeout only gets used when switching from unlocked
to locked, and is replaced by the regular "timeout when locked"
thereafter until the session gets unlocked again.

Suspend/resume handling receives some extra care to behave well.
Unlocked timeouts also apply a minimum duration for extra safety.

daemon/actions/bundled/dpms.cpp
@@ -18,7 +18,6 @@
 #include <QDBusConnection>
 #include <QDBusMessage>
 #include <QDBusPendingCall>
-#include <QDebug>
 #include <QGuiApplication>
 #include <QTimer>
 #include <private/qtx11extras_p.h>
@@ -33,6 +32,11 @@
 K_PLUGIN_CLASS_WITH_JSON(PowerDevil::BundledActions::DPMS, "powerdevildpmsaction.json")
 
 using namespace std::chrono_literals;
+
+// always make sure to allow meaningful interaction and for lock screens, time for authentication methods
+static constexpr std::chrono::milliseconds s_minIdleTimeoutWhenUnlocked = 30s;
+static constexpr std::chrono::milliseconds s_minIdleTimeoutWhenLocked = 10s;
+static constexpr std::chrono::milliseconds s_minIdleTimeoutWhenActivatingLock = 100ms;
 
 namespace PowerDevil
 {
@@ -58,8 +62,10 @@ DPMS::DPMS(QObject *parent)
     // Listen to the policy agent
     auto policyAgent = PowerDevil::PolicyAgent::instance();
     connect(policyAgent, &PowerDevil::PolicyAgent::unavailablePoliciesChanged, this, &DPMS::onUnavailablePoliciesChanged);
-
     connect(policyAgent, &PowerDevil::PolicyAgent::screenLockerActiveChanged, this, &DPMS::onScreenLockerActiveChanged);
+
+    connect(core()->suspendController(), &SuspendController::aboutToSuspend, this, &DPMS::onAboutToSuspend);
+    connect(core()->suspendController(), &SuspendController::resumeFromSuspend, this, &DPMS::onResumeFromSuspend);
 
     // inhibitions persist over kded module unload/load
     m_inhibitScreen = policyAgent->unavailablePolicies() & PowerDevil::PolicyAgent::ChangeScreenSettings;
@@ -97,6 +103,10 @@ bool DPMS::isSupported()
 
 void DPMS::onWakeupFromIdle()
 {
+    if (m_isActivatingLock) {
+        // Use the longer minimum timeout whenever we see user interaction, e.g. for allowing the user to type
+        registerStandardIdleTimeout();
+    }
     Q_EMIT stopFade(); // only actively used in X11
 
     if (m_oldKeyboardBrightness > 0) {
@@ -168,13 +178,20 @@ bool DPMS::loadAction(const PowerDevil::ProfileSettings &profileSettings)
         return false;
     }
 
-    m_idleTime = std::chrono::seconds(profileSettings.turnOffDisplayIdleTimeoutSec());
+    m_idleTimeoutWhenUnlocked = std::chrono::seconds(profileSettings.turnOffDisplayIdleTimeoutSec());
+    m_idleTimeoutWhenLocked = std::chrono::seconds(profileSettings.turnOffDisplayIdleTimeoutWhenLockedSec());
     m_lockBeforeTurnOff = profileSettings.lockBeforeTurnOffDisplay();
 
-    m_idleTimeoutWhenLocked = std::chrono::seconds(profileSettings.turnOffDisplayIdleTimeoutWhenLockedSec());
+    if (m_idleTimeoutWhenUnlocked >= 0ms) {
+        m_idleTimeoutWhenUnlocked = std::max(m_idleTimeoutWhenUnlocked, s_minIdleTimeoutWhenUnlocked);
+    }
+    if (m_idleTimeoutWhenLocked >= 0ms) {
+        // Allow (almost) immediate turn-off upon entering the lockscreen, but not when already locked
+        m_idleTimeoutWhenActivatingLock = std::max(m_idleTimeoutWhenLocked, s_minIdleTimeoutWhenActivatingLock);
+        m_idleTimeoutWhenLocked = std::max(m_idleTimeoutWhenLocked, s_minIdleTimeoutWhenLocked);
+    }
 
-    registerDpmsOffOnIdleTimeout(m_idleTime);
-
+    registerStandardIdleTimeout();
     return true;
 }
 
@@ -183,24 +200,43 @@ void DPMS::onUnavailablePoliciesChanged(PowerDevil::PolicyAgent::RequiredPolicie
     m_inhibitScreen = policies & PowerDevil::PolicyAgent::ChangeScreenSettings;
 }
 
-void PowerDevil::BundledActions::DPMS::registerDpmsOffOnIdleTimeout(std::chrono::milliseconds timeout)
+void DPMS::registerStandardIdleTimeout()
 {
-    if (timeout > 0s) {
-        registerIdleTimeout(timeout);
+    unregisterIdleTimeouts();
+    m_isActivatingLock = false;
+
+    if (PowerDevil::PolicyAgent::instance()->screenLockerActive()) {
+        registerIdleTimeout(m_idleTimeoutWhenLocked);
+    } else {
+        registerIdleTimeout(m_idleTimeoutWhenUnlocked);
     }
 }
 
 void DPMS::onScreenLockerActiveChanged(bool active)
 {
-    unloadAction();
+    unregisterIdleTimeouts();
+    m_isActivatingLock = active && !m_isAboutToSuspend;
 
-    if (active) {
-        // in lockscreen
-        registerDpmsOffOnIdleTimeout(m_idleTimeoutWhenLocked);
-    } else {
+    if (m_isActivatingLock) {
+        // entering lockscreen - fast display turn-off if the config calls for it
+        registerIdleTimeout(m_idleTimeoutWhenActivatingLock);
+        // if we're locking but about to suspend, wait until onResumeFromSuspend() to register the timeout
+    } else if (!active) {
         // restoring normal idleTimeout
-        registerDpmsOffOnIdleTimeout(m_idleTime);
+        registerIdleTimeout(m_idleTimeoutWhenUnlocked);
     }
+}
+
+void DPMS::onAboutToSuspend()
+{
+    m_isAboutToSuspend = true;
+    unregisterIdleTimeouts();
+}
+
+void DPMS::onResumeFromSuspend()
+{
+    m_isAboutToSuspend = false;
+    registerStandardIdleTimeout();
 }
 
 static std::chrono::milliseconds dimAnimationTime()

daemon/actions/bundled/dpms.h
@@ -47,13 +47,18 @@ private Q_SLOTS:
     void turnOffOnIdleTimeout();
     void onUnavailablePoliciesChanged(PowerDevil::PolicyAgent::RequiredPolicies policies);
     void onScreenLockerActiveChanged(bool active);
+    void onAboutToSuspend();
+    void onResumeFromSuspend();
 
 private:
     void setKeyboardBrightnessHelper(int brightness);
-    void registerDpmsOffOnIdleTimeout(std::chrono::milliseconds timeout);
+    void registerStandardIdleTimeout();
 
-    std::chrono::seconds m_idleTime{-1};
-    std::chrono::seconds m_idleTimeoutWhenLocked{-1};
+    std::chrono::milliseconds m_idleTimeoutWhenUnlocked{-1};
+    std::chrono::milliseconds m_idleTimeoutWhenLocked{-1};
+    std::chrono::milliseconds m_idleTimeoutWhenActivatingLock{-1};
+    bool m_isActivatingLock = false;
+    bool m_isAboutToSuspend = false;
     PowerDevil::PolicyAgent::RequiredPolicies m_inhibitScreen = PowerDevil::PolicyAgent::None;
 
     int m_oldKeyboardBrightness = 0;

daemon/powerdevilaction.cpp
@@ -30,11 +30,16 @@ void Action::registerIdleTimeout(std::chrono::milliseconds timeout)
     m_core->registerActionTimeout(this, timeout);
 }
 
-void Action::unloadAction()
+void Action::unregisterIdleTimeouts()
 {
     // Remove all registered idle timeouts, if any
     m_core->unregisterActionTimeouts(this);
     m_registeredIdleTimeouts.clear();
+}
+
+void Action::unloadAction()
+{
+    unregisterIdleTimeouts();
 }
 
 bool Action::isSupported()

daemon/powerdevilaction.h
@@ -11,6 +11,8 @@
 #include <QObject>
 #include <QVariantMap>
 
+#include <chrono>
+
 #include "powerdevilcore_export.h"
 
 namespace PowerDevil
@@ -126,6 +128,10 @@ protected:
      * to KIdleTime's signals to catch idle timeout events.
      */
     void registerIdleTimeout(std::chrono::milliseconds timeout);
+    /**
+     * Removes any previously registered idle timeouts for this action.
+     */
+    void unregisterIdleTimeouts();
     /**
      * Sets the required policies needed for this Action to run. Usually, you want to call this function in your
      * Action's constructor.

commit be2b83615d879549ecf9a47b66dc66887419a2dc

actions/dpms: Ignore turn-off triggers when action is disabled
Early Plasma 6.0 releases saw many people reporting unintentional
screen turn-off when the screen locker activates/deactivates,
and when the system wakes up from sleep. On X11, this was visible
to the user immediately, whereas on Wayland it spammed system logs
with warnings of invalid -1 idle timeout registrations.

Ironically, this behavior would occur specifically when the DPMS
action (a.k.a. "When locked, turn screen off") was disabled.

The reason is that the DPMS object gets created either way, and
sets up its screen locker activation change handler as well as
suspend/resume handlers in the constructor. But timeout values
can remain invalid until the action is loaded/enabled and
timeout values are populated from profile settings.
Using invalid timeouts in these handlers caused this headache.

This bug was introduced by commit c58085b4, which fixed a bunch of
things, bug also removed checks for invalid timeout values.
Turns out we still need some kind of checks.

We now prevent bad timeout registrations by interpreting negative
values in m_idleTimeoutWhenUnlocked as "idle timeout disabled".
Checks for this value ensure that registerIdleTimeout() is only
called when the action is loaded, regardless of whether it's
triggered by screen locker changing its activation status,
resume after suspend, or any other event.

Alternatively, we could have also moved some signal connections
into loadAction() and disconnected them in onProfileUnload().
Checking on every registration call seems more robust though.

BUG: 481308
BUG: 482077

daemon/actions/bundled/dpms.cpp
@@ -171,25 +171,31 @@ void DPMS::triggerImpl(const QVariantMap &args)
 
 bool DPMS::loadAction(const PowerDevil::ProfileSettings &profileSettings)
 {
-    if (!profileSettings.turnOffDisplayWhenIdle()) {
-        return false;
-    }
-
     m_idleTimeoutWhenUnlocked = std::chrono::seconds(profileSettings.turnOffDisplayIdleTimeoutSec());
     m_idleTimeoutWhenLocked = std::chrono::seconds(profileSettings.turnOffDisplayIdleTimeoutWhenLockedSec());
     m_lockBeforeTurnOff = profileSettings.lockBeforeTurnOffDisplay();
 
-    if (m_idleTimeoutWhenUnlocked >= 0ms) {
-        m_idleTimeoutWhenUnlocked = std::max(m_idleTimeoutWhenUnlocked, s_minIdleTimeoutWhenUnlocked);
+    if (!profileSettings.turnOffDisplayWhenIdle() || m_idleTimeoutWhenUnlocked < 0ms) {
+        onProfileUnload();
+        return false;
     }
-    if (m_idleTimeoutWhenLocked >= 0ms) {
-        // Allow (almost) immediate turn-off upon entering the lockscreen, but not when already locked
-        m_idleTimeoutWhenActivatingLock = std::max(m_idleTimeoutWhenLocked, s_minIdleTimeoutWhenActivatingLock);
-        m_idleTimeoutWhenLocked = std::max(m_idleTimeoutWhenLocked, s_minIdleTimeoutWhenLocked);
+
+    m_idleTimeoutWhenUnlocked = std::max(m_idleTimeoutWhenUnlocked, s_minIdleTimeoutWhenUnlocked);
+
+    if (m_idleTimeoutWhenLocked < 0ms) {
+        m_idleTimeoutWhenLocked = m_idleTimeoutWhenUnlocked;
     }
+    // Allow (almost) immediate turn-off upon entering the lockscreen, but not when already locked
+    m_idleTimeoutWhenActivatingLock = std::max(m_idleTimeoutWhenLocked, s_minIdleTimeoutWhenActivatingLock);
+    m_idleTimeoutWhenLocked = std::max(m_idleTimeoutWhenLocked, s_minIdleTimeoutWhenLocked);
 
     registerStandardIdleTimeout();
     return true;
+}
+
+void DPMS::onProfileUnload()
+{
+    m_idleTimeoutWhenUnlocked = std::chrono::milliseconds(-1);
 }
 
 void DPMS::onUnavailablePoliciesChanged(PowerDevil::PolicyAgent::RequiredPolicies policies)
@@ -202,6 +208,10 @@ void DPMS::registerStandardIdleTimeout()
     unregisterIdleTimeouts();
     m_isActivatingLock = false;
 
+    if (m_idleTimeoutWhenUnlocked < 0ms) { // action created, but not loaded
+        return;
+    }
+
     if (PowerDevil::PolicyAgent::instance()->screenLockerActive()) {
         registerIdleTimeout(m_idleTimeoutWhenLocked);
     } else {
@@ -213,6 +223,10 @@ void DPMS::onScreenLockerActiveChanged(bool active)
 {
     unregisterIdleTimeouts();
     m_isActivatingLock = active && !m_isAboutToSuspend;
+
+    if (m_idleTimeoutWhenUnlocked < 0ms) { // action created, but not loaded
+        return;
+    }
 
     if (m_isActivatingLock) {
         // entering lockscreen - fast display turn-off if the config calls for it

daemon/actions/bundled/dpms.h
@@ -39,6 +39,7 @@ protected:
     void onIdleTimeout(std::chrono::milliseconds timeout) override;
     void triggerImpl(const QVariantMap &args) override;
     bool isSupported() override;
+    void onProfileUnload() override;
 
 public:
     bool loadAction(const PowerDevil::ProfileSettings &profileSettings) override;