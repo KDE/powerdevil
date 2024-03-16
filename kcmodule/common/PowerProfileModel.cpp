KDE/powerdevil

commit 8ecf6430ee7a3d3670fd9cb1b6af947f9ac2f352

kcmodule: Don't crash when a configured button action is unsupported
The bounds check in the data() method of PowerButtonActionModel
and SleepModeModel was incomplete, so passing an invalid index
such as -1 (standard value for "not found") could cause a crash.

For power management settings, a user would run into this crash if
their configuration from powerdevilrc listed a power button action
that's not supported on the current system. For example, a user
may have selected "Sleep" as lid action, but subsequently changed
their systemd configuration to disable suspend-to-RAM because it
caused problems. Or they could have migrated their OS to a new
computer which does not support Sleep.

Fixing this allows us to assign currentIndex declaratively
instead of waiting until Component.onCompleted to (hopefully) find
a valid item in the model.

BUG: 482668

kcmodule/profiles/ui/GlobalConfig.qml
@@ -152,12 +152,11 @@ Kirigami.ScrollablePage {
             textRole: "name"
             valueRole: "value"
 
+            currentIndex: indexOfValue(globalSettings.batteryCriticalAction)
+
             KCM.SettingStateBinding {
                 configObject: globalSettings
                 settingName: "BatteryCriticalAction"
-            }
-            Component.onCompleted: {
-                currentIndex = Qt.binding(() => indexOfValue(globalSettings.batteryCriticalAction));
             }
             onActivated: {
                 globalSettings.batteryCriticalAction = currentValue;

kcmodule/profiles/ui/ProfileConfig.qml
@@ -64,12 +64,11 @@ Kirigami.FormLayout {
             textRole: "name"
             valueRole: "value"
 
+            currentIndex: indexOfValue(profileSettings.autoSuspendAction)
+
             KCM.SettingStateBinding {
                 configObject: profileSettings
                 settingName: "AutoSuspendAction"
-            }
-            Component.onCompleted: {
-                currentIndex = Qt.binding(() => indexOfValue(profileSettings.autoSuspendAction));
             }
             onActivated: {
                 profileSettings.autoSuspendAction = currentValue;
@@ -112,12 +111,11 @@ Kirigami.FormLayout {
         textRole: "name"
         valueRole: "value"
 
+        currentIndex: indexOfValue(profileSettings.powerButtonAction)
+
         KCM.SettingStateBinding {
             configObject: profileSettings
             settingName: "PowerButtonAction"
-        }
-        Component.onCompleted: {
-            currentIndex = Qt.binding(() => indexOfValue(profileSettings.powerButtonAction));
         }
         onActivated: {
             profileSettings.powerButtonAction = currentValue;
@@ -140,12 +138,11 @@ Kirigami.FormLayout {
         textRole: "name"
         valueRole: "value"
 
+        currentIndex: indexOfValue(profileSettings.lidAction)
+
         KCM.SettingStateBinding {
             configObject: profileSettings
             settingName: "LidAction"
-        }
-        Component.onCompleted: {
-            currentIndex = Qt.binding(() => indexOfValue(profileSettings.lidAction));
         }
         onActivated: {
             profileSettings.lidAction = currentValue;
@@ -201,6 +198,8 @@ Kirigami.FormLayout {
             highlighted: index === sleepModeCombo.currentIndex
         }
 
+        currentIndex: indexOfValue(profileSettings.sleepMode)
+
         KCM.SettingStateBinding {
             configObject: profileSettings
             settingName: "SleepMode"
@@ -209,9 +208,6 @@ Kirigami.FormLayout {
                 || powerButtonActionCombo.currentValue === PD.PowerDevil.PowerButtonAction.Sleep
                 || lidActionCombo.currentValue === PD.PowerDevil.PowerButtonAction.Sleep
             )
-        }
-        Component.onCompleted: {
-            currentIndex = Qt.binding(() => indexOfValue(profileSettings.sleepMode));
         }
         onActivated: {
             profileSettings.sleepMode = currentValue;

kcmodule/common/PowerButtonActionModel.cpp
@@ -97,7 +97,7 @@ void PowerButtonActionModel::appendAction(PowerDevil::PowerButtonAction action, 
 
 QVariant PowerButtonActionModel::data(const QModelIndex &index, int role) const
 {
-    if (index.row() > m_data.size()) {
+    if (index.row() < 0 || index.row() >= m_data.size()) {
         return {};
     }
 

kcmodule/common/SleepModeModel.cpp
@@ -43,7 +43,7 @@ SleepModeModel::SleepModeModel(QObject *parent, PowerDevil::PowerManagement *pm)
 
 QVariant SleepModeModel::data(const QModelIndex &index, int role) const
 {
-    if (index.row() > m_data.size()) {
+    if (index.row() < 0 || index.row() >= m_data.size()) {
         return {};
     }
 

commit 78415ae0097ad36377b407d3b143589ca8fa8a18

kcmodule: Revert to binding currentIndex in Component.onCompleted
Commit 8ecf6430 ("Don't crash when a configured button action
is unsupported") caused empty ComboBox selections on initial load.
Turns out `indexOfValue()` can only be used after the
Component.completed() signal is emitted, so a purely declarative
binding worked incorrectly if queried too early by QML.

Therefore, we revert back to assigning a Qt.binding() to
currentIndex in Component.onCompleted.

This commit also adapts PowerProfileModel and its associated
ComboBox in a similar way as the other two models used in this KCM.

BUG: 483750
CCBUG: 482668

kcmodule/common/PowerProfileModel.cpp
@@ -58,7 +58,7 @@ PowerProfileModel::PowerProfileModel(QObject *parent)
 
 QVariant PowerProfileModel::data(const QModelIndex &index, int role) const
 {
-    if (index.row() > m_data.size()) {
+    if (index.row() < 0 || index.row() >= m_data.size()) {
         return {};
     }
 

kcmodule/profiles/ui/GlobalConfig.qml
@@ -160,12 +160,15 @@ Kirigami.ScrollablePage {
             textRole: "name"
             valueRole: "value"
 
-            currentIndex: indexOfValue(globalSettings.batteryCriticalAction)
             readonly property bool isConfiguredValueSupported: currentValue === globalSettings.batteryCriticalAction
 
             KCM.SettingStateBinding {
                 configObject: globalSettings
                 settingName: "BatteryCriticalAction"
+            }
+            Component.onCompleted: {
+                // indexOfValue() is invalid before onCompleted, so wait until here to bind currentIndex.
+                currentIndex = Qt.binding(() => indexOfValue(globalSettings.batteryCriticalAction));
             }
             onActivated: {
                 globalSettings.batteryCriticalAction = currentValue;

kcmodule/profiles/ui/ProfileConfig.qml
@@ -72,12 +72,15 @@ Kirigami.FormLayout {
             textRole: "name"
             valueRole: "value"
 
-            currentIndex: indexOfValue(profileSettings.autoSuspendAction)
             readonly property bool isConfiguredValueSupported: currentValue === profileSettings.autoSuspendAction
 
             KCM.SettingStateBinding {
                 configObject: profileSettings
                 settingName: "AutoSuspendAction"
+            }
+            Component.onCompleted: {
+                // indexOfValue() is invalid before onCompleted, so wait until here to bind currentIndex.
+                currentIndex = Qt.binding(() => indexOfValue(profileSettings.autoSuspendAction));
             }
             onActivated: {
                 profileSettings.autoSuspendAction = currentValue;
@@ -128,12 +131,15 @@ Kirigami.FormLayout {
         textRole: "name"
         valueRole: "value"
 
-        currentIndex: indexOfValue(profileSettings.powerButtonAction)
         readonly property bool isConfiguredValueSupported: currentValue === profileSettings.powerButtonAction
 
         KCM.SettingStateBinding {
             configObject: profileSettings
             settingName: "PowerButtonAction"
+        }
+        Component.onCompleted: {
+            // indexOfValue() is invalid before onCompleted, so wait until here to bind currentIndex.
+            currentIndex = Qt.binding(() => indexOfValue(profileSettings.powerButtonAction));
         }
         onActivated: {
             profileSettings.powerButtonAction = currentValue;
@@ -164,12 +170,15 @@ Kirigami.FormLayout {
         textRole: "name"
         valueRole: "value"
 
-        currentIndex: indexOfValue(profileSettings.lidAction)
         readonly property bool isConfiguredValueSupported: currentValue === profileSettings.lidAction
 
         KCM.SettingStateBinding {
             configObject: profileSettings
             settingName: "LidAction"
+        }
+        Component.onCompleted: {
+            // indexOfValue() is invalid before onCompleted, so wait until here to bind currentIndex.
+            currentIndex = Qt.binding(() => indexOfValue(profileSettings.lidAction));
         }
         onActivated: {
             profileSettings.lidAction = currentValue;
@@ -233,7 +242,6 @@ Kirigami.FormLayout {
             highlighted: index === sleepModeCombo.currentIndex
         }
 
-        currentIndex: indexOfValue(profileSettings.sleepMode)
         readonly property bool isConfiguredValueSupported: currentValue === profileSettings.sleepMode
 
         KCM.SettingStateBinding {
@@ -244,6 +252,10 @@ Kirigami.FormLayout {
                 || powerButtonActionCombo.currentValue === PD.PowerDevil.PowerButtonAction.Sleep
                 || lidActionCombo.currentValue === PD.PowerDevil.PowerButtonAction.Sleep
             )
+        }
+        Component.onCompleted: {
+            // indexOfValue() is invalid before onCompleted, so wait until here to bind currentIndex.
+            currentIndex = Qt.binding(() => indexOfValue(profileSettings.sleepMode));
         }
         onActivated: {
             profileSettings.sleepMode = currentValue;
@@ -490,17 +502,14 @@ Kirigami.FormLayout {
         textRole: "name"
         valueRole: "value"
 
-        /*private*/ property bool assignedInitialIndex: false
-
         KCM.SettingStateBinding {
             configObject: profileSettings
             settingName: "PowerProfile"
         }
-        onCountChanged: { // PowerProfileModel has delayed initialization due to a D-Bus call
-            if (count > 0 && !assignedInitialIndex) {
-                currentIndex = Qt.binding(() => indexOfValue(profileSettings.powerProfile));
-                assignedInitialIndex = true;
-            }
+        Component.onCompleted: {
+            // indexOfValue() is invalid before onCompleted, so wait until here to bind currentIndex.
+            // Also observe count - PowerProfileModel has delayed initialization due to a D-Bus call.
+            currentIndex = Qt.binding(() => count ? indexOfValue(profileSettings.powerProfile) : -1);
         }
         onActivated: {
             profileSettings.powerProfile = currentValue;