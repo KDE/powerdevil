KDE/powerdevil

commit 584cfdf0256bc7034be0b3fadf94f7b486597aa6

core: reload actions on brightness controller changes
Whether or not the brightness control action is supported depends on the currently
connected list of screens, which is dynamic and can't just be checked on startup.
To fix this, reload actions when devices get added or removed

CCBUG: 482713

daemon/powerdevilcore.cpp
@@ -91,6 +91,8 @@ void Core::loadCore()
     m_screenBrightnessController = std::make_unique<ScreenBrightnessController>();
 
     connect(m_screenBrightnessController.get(), &ScreenBrightnessController::detectionFinished, this, &Core::onControllersReady);
+    connect(m_screenBrightnessController.get(), &ScreenBrightnessController::displayAdded, this, &Core::refreshActions);
+    connect(m_screenBrightnessController.get(), &ScreenBrightnessController::displayRemoved, this, &Core::refreshActions);
     m_screenBrightnessController->detectDisplays();
 }
 
@@ -157,7 +159,7 @@ void Core::onControllersReady()
     });
 
     // Initialize the action pool, which will also load the needed startup actions.
-    initActions();
+    refreshActions();
 
     // Set up the critical battery timer
     m_criticalBatteryTimer->setSingleShot(true);
@@ -207,34 +209,28 @@ void Core::onControllersReady()
     refreshStatus();
 }
 
-void Core::initActions()
+void Core::refreshActions()
 {
     const QList<KPluginMetaData> offers = KPluginMetaData::findPlugins(QStringLiteral("powerdevil/action"));
     for (const KPluginMetaData &data : offers) {
-        if (auto plugin = KPluginFactory::instantiatePlugin<PowerDevil::Action>(data, this).plugin) {
-            m_actionPool.insert(data.value(QStringLiteral("X-KDE-PowerDevil-Action-ID")), plugin);
+        const QString actionId = data.value(QStringLiteral("X-KDE-PowerDevil-Action-ID"));
+        if (m_actionPool.contains(actionId)) {
+            continue;
+        }
+        std::unique_ptr<PowerDevil::Action> action{KPluginFactory::instantiatePlugin<PowerDevil::Action>(data, this).plugin};
+        if (action && action->isSupported()) {
+            if (data.value(QStringLiteral("X-KDE-PowerDevil-Action-RegistersDBusInterface"), false)) {
+                QDBusConnection::sessionBus().registerObject(QStringLiteral("/org/kde/Solid/PowerManagement/Actions/") + actionId, action.get());
+            }
+            m_actionPool.emplace(actionId, std::move(action));
         }
     }
 
-    // Verify support
-    QHash<QString, Action *>::iterator i = m_actionPool.begin();
-    while (i != m_actionPool.end()) {
-        Action *action = i.value();
-        if (!action->isSupported()) {
-            i = m_actionPool.erase(i);
-            action->deleteLater();
-        } else {
-            ++i;
-        }
-    }
-
-    // Register DBus objects
-    for (const KPluginMetaData &offer : offers) {
-        QString actionId = offer.value(QStringLiteral("X-KDE-PowerDevil-Action-ID"));
-        if (offer.value(QStringLiteral("X-KDE-PowerDevil-Action-RegistersDBusInterface"), false) && m_actionPool.contains(actionId)) {
-            QDBusConnection::sessionBus().registerObject(QStringLiteral("/org/kde/Solid/PowerManagement/Actions/") + actionId, m_actionPool[actionId]);
-        }
-    }
+    // Remove now-unsupported actions
+    std::erase_if(m_actionPool, [](const auto &pair) {
+        const auto &[name, action] = pair;
+        return !action->isSupported();
+    });
 }
 
 bool Core::isActionSupported(const QString &actionName)
@@ -361,10 +357,10 @@ void Core::loadProfile(bool force)
         }
 
         // Cool, now let's load the needed actions. Mark the ones as active that want to be loaded
-        for (auto it = m_actionPool.begin(); it != m_actionPool.end(); ++it) {
-            if (it.value()->loadAction(profileSettings)) {
-                m_activeActions.append(it.key());
-                it.value()->onProfileLoad(m_currentProfile, profileId);
+        for (const auto &[id, action] : m_actionPool) {
+            if (action->loadAction(profileSettings)) {
+                m_activeActions.append(id);
+                action->onProfileLoad(m_currentProfile, profileId);
             }
         }
 
@@ -991,7 +987,8 @@ BatteryController *Core::batteryController()
 
 Action *Core::action(const QString actionId)
 {
-    return m_actionPool.value(actionId, nullptr);
+    const auto it = m_actionPool.find(actionId);
+    return it == m_actionPool.end() ? nullptr : it->second.get();
 }
 
 void Core::unloadAllActiveActions()

daemon/powerdevilcore.h
@@ -94,6 +94,7 @@ public Q_SLOTS:
     void refreshStatus();
     void reparseConfiguration();
     void onControllersReady();
+    void refreshActions();
 
     QString currentProfile() const;
     void loadProfile(bool force = false);
@@ -129,7 +130,6 @@ Q_SIGNALS:
     void chargeStopThresholdChanged(int threshold);
 
 private:
-    void initActions();
     void unloadAllActiveActions();
     void registerActionTimeout(Action *action, std::chrono::milliseconds timeout);
     void unregisterActionTimeouts(Action *action);
@@ -192,7 +192,7 @@ private:
     QHash<QString, int> m_sessionActivityInhibit;
     QHash<QString, int> m_screenActivityInhibit;
 
-    QHash<QString, Action *> m_actionPool;
+    std::unordered_map<QString, std::unique_ptr<Action>> m_actionPool;
     QStringList m_activeActions;
 
 private Q_SLOTS:

commit 41b4ca1dc515b1e44b31f6c4b5eadeb8571cd8f8

daemon: Don't crash in PowerDevil::Core::unloadAllActiveActions()
Commit 584cfdf0 introduced the daemon's ability to remove actions
from `Core::m_actionPool` if they become unsupported at runtime.
Commit 7a929fa0 fixed some resulting crashes by also cleaning up
related map/set elements that contained this action.
That was not enough, we got lots of crashes coming in since then.

The (many) remaining crashes happened in `unloadAllActiveActions()`,
which cycles through `Core::m_activeActions` and accesses the
corresponding action pointer in `m_actionPool`. The implication is
that `m_activeActions` contains action IDs that are not present
in `m_actionPool`. And indeed, commit 7a929fa0 missed cleaning up
the action ID corresponding to the now-unsupported action.

With the dangling ID removed, PowerDevil should finally stop crashing.

SENTRY: FALLTHROUGH-30F
BUG: 492349

daemon/powerdevilcore.cpp
@@ -230,10 +230,10 @@ void Core::refreshActions()
 
     // Remove now-unsupported actions
     for (auto it = m_actionPool.begin(); it != m_actionPool.end();) {
-        Action *action = it->second.get();
-        if (!action->isSupported()) {
-            m_registeredActionTimeouts.remove(action);
-            m_pendingResumeFromIdleActions.remove(action);
+        if (const auto &[actionId, action] = *it; !action->isSupported()) {
+            m_activeActions.removeOne(actionId);
+            m_registeredActionTimeouts.remove(action.get());
+            m_pendingResumeFromIdleActions.remove(action.get());
             it = m_actionPool.erase(it);
             actionsChanged = true;
         } else {