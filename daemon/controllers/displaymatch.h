2adc2caf58e108891da16c2922b76598fe47d024
Jakob Petsovits <jpetso@petsovits.com>
2025-01-09 01:20:22 -0500

    daemon/controllers: Introduce DisplayFilter helper class
    Unlike the previously added DisplayMatch class, this can be used to
    match more than one display, every matching criterion is optional.
    A default-constructed filter includes all displays.
    
    For now, it merely replaces similar checks in
    `ScreenBrightnessController::tryMatchKScreenOutput()`.
    The plan is to use it for dimming limit constraints as well.

--- daemon/controllers/displaymatch.h
+++ daemon/controllers/displaymatch.h
@@ -11,8 +11,8 @@
 #include <optional>
 
 /**
- * Stores data that can be used to match displays well enough (alas, not perfectly) across
- * reboots and reconnects.
+ * Stores data that can be used to match a single display well enough (alas, not perfectly)
+ * across reboots and reconnects.
  */
 struct DisplayMatch {
     int maxBrightness = 0;
@@ -35,4 +35,66 @@ struct DisplayMatch {
     bool operator>(const DisplayMatch &other) const = default;
     bool operator<=(const DisplayMatch &other) const = default;
     bool operator>=(const DisplayMatch &other) const = default;
+};
+
+/**
+ * Stores data that can be used to match one or more displays as part of a rule-driven behavior.
+ */
+struct DisplayFilter {
+    std::optional<bool> isInternal;
+    QList<QByteArray> includedEdids;
+    QList<QByteArray> excludedEdids;
+    bool includedByDefault = true;
+
+    bool includes(const DisplayMatch &match) const
+    {
+        if (isInternal.has_value() && *isInternal != match.isInternal) {
+            return false;
+        }
+        for (const QByteArray &includedEdid : std::as_const(includedEdids)) {
+            if (includedEdid.isEmpty() || !match.edidData.has_value() || match.edidData->isEmpty()) {
+                continue;
+            }
+            const bool isPrefixMatch = includedEdid.size() < match.edidData->size() // e.g. first 128 bytes vs. full EDID
+                ? match.edidData->startsWith(includedEdid)
+                : includedEdid.startsWith(*match.edidData);
+            if (isPrefixMatch) {
+                return true;
+            }
+        }
+        for (const QByteArray &excludedEdid : std::as_const(excludedEdids)) {
+            if (excludedEdid.isEmpty() || !match.edidData.has_value() || match.edidData->isEmpty()) {
+                continue;
+            }
+            const bool isPrefixMatch = excludedEdid.size() < match.edidData->size() // e.g. first 128 bytes vs. full EDID
+                ? match.edidData->startsWith(excludedEdid)
+                : excludedEdid.startsWith(*match.edidData);
+            if (isPrefixMatch) {
+                return false;
+            }
+        }
+        return includedByDefault;
+    }
+
+    // fluent API for quick & sufficiently verbose initialization
+    DisplayFilter &includeByDefault(bool b)
+    {
+        includedByDefault = b;
+        return *this;
+    }
+    DisplayFilter &isInternalEquals(bool b)
+    {
+        isInternal = b;
+        return *this;
+    }
+    DisplayFilter &includeEdids(QList<QByteArray> edids)
+    {
+        includedEdids = std::move(edids);
+        return *this;
+    }
+    DisplayFilter &excludeEdids(QList<QByteArray> edids)
+    {
+        excludedEdids = std::move(edids);
+        return *this;
+    }
 };
