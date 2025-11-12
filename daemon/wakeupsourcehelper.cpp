e49f9ae776a35016f2f280e752613db9c56df688
Bhushan Shah <bshah@kde.org>
2025-11-12 11:10:39 +0530

    daemon: perform dark resume on s2idle mode only
    Currently there are some platform specific bugs with deep suspend and in
    specific x86 platforms, which causes wakeup event during suspend
    process. This causes it to fallback to s2idle mode.
    
    Limit dark resume mode by reading current mem sleep mode, and if it is
    not s2idle, we skip writing wakeup count file.
    
    BUG: 510992

--- daemon/wakeupsourcehelper.cpp
+++ daemon/wakeupsourcehelper.cpp
@@ -10,10 +10,12 @@
 
 #include <QDir>
 #include <QFile>
+#include <QRegularExpression>
 
 using namespace Qt::StringLiterals;
 
 static const QString s_wakeupCountSysFsPath = QStringLiteral("/sys/power/wakeup_count");
+static const QString s_memSleepModeSysFsPath = QStringLiteral("/sys/power/mem_sleep");
 
 WakeupSourceHelper::WakeupSourceHelper(QObject *parent)
     : QObject(parent)
@@ -24,9 +26,33 @@ ActionReply WakeupSourceHelper::setwakeupcount(const QVariantMap &args)
 {
     Q_UNUSED(args)
 
-    QFile file(s_wakeupCountSysFsPath);
+    // First check if current suspend mode is s2idle, then only proceed further
+    QFile memSleepFile(s_memSleepModeSysFsPath);
+    if (!memSleepFile.open(QIODevice::ReadOnly)) {
+        auto reply = ActionReply::HelperErrorReply();
+        reply.setErrorDescription(QStringLiteral("Failed to get current sleep mode, skipping wakeup source helper"));
+        return reply;
+    }
+
+    QString sleepMode;
+    QTextStream memSleepStream(&memSleepFile);
+
+    // text will be in form of "s2idle [deep]", we want to check current mode.
+    QRegularExpression re(uR"(\[(\w+)\])"_s);
+    QRegularExpressionMatch match = re.match(memSleepStream.readLine());
+    if (match.hasMatch()) {
+        sleepMode = match.captured(1);
+    }
+
+    if (sleepMode != QLatin1StringView("s2idle")) {
+        // Since currently there are platform bugs with deep mode
+        // let's skip writing wakeup count.
+        return ActionReply::SuccessReply();
+    }
+
+    QFile wakeupFile(s_wakeupCountSysFsPath);
     // If wakeup_count can not be opened, it may mean something is running suspend already
-    if (!file.open(QIODevice::ReadWrite)) {
+    if (!wakeupFile.open(QIODevice::ReadWrite)) {
         auto reply = ActionReply::HelperErrorReply();
         reply.setErrorDescription(QStringLiteral("Failed to open wakeup_count, suspend maybe already in progress"));
         return reply;
@@ -34,11 +60,11 @@ ActionReply WakeupSourceHelper::setwakeupcount(const QVariantMap &args)
 
     // Read current wakeup count
     int wakeupCount = -1;
-    QTextStream stream(&file);
+    QTextStream stream(&wakeupFile);
     stream >> wakeupCount;
 
     // write same count
-    if (file.write(QByteArray::number(wakeupCount)) == -1) {
+    if (wakeupFile.write(QByteArray::number(wakeupCount)) == -1) {
         auto reply = ActionReply::HelperErrorReply();
         reply.setErrorDescription(QStringLiteral("Failed to write wakeup_count, wakeup_count may have changed in between"));
         return reply;
