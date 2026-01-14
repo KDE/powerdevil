9dff9363573a910a7dd26d927b879e9d7ad39ee8
Jakob Petsovits <jpetso@petsovits.com>
2026-01-15 00:09:51 +0100

    daemon: Log the absence of backlight devices as info, not warning
    BUG: 445426

--- daemon/controllers/backlightbrightness_linux.cpp
+++ daemon/controllers/backlightbrightness_linux.cpp
@@ -58,7 +58,7 @@ void BacklightDetector::detect()
     } else if (QList<BacklightSysfsDevice> devices = BacklightSysfsDevice::getBacklightTypeDevices(); !devices.isEmpty()) {
         m_display.reset(new BacklightBrightness(std::move(devices), this));
     } else {
-        qCWarning(POWERDEVIL_BACKLIGHTBRIGHTNESS) << "not supported: no kernel backlight interface found";
+        qCInfo(POWERDEVIL_BACKLIGHTBRIGHTNESS) << "not supported: no kernel backlight interface found";
     }
     Q_EMIT detectionFinished(m_display != nullptr);
 }
