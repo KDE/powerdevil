KDE/powerdevil

commit 94a03a3c04787388026a02ff40afd2b77fc9d0f6

BrightnessItem: only do slider->Upower when dragging the slider
don't bind directly the slider value to the value coming from upower
but do a conditional binding that is active only when not dragging.
when dragging only write root.value and don't read to value at all,
and only read when the user is not moving the slider

CCBUG:520160

applets/brightness/BrightnessItem.qml
@@ -20,11 +20,16 @@ PlasmaComponents3.ItemDelegate {
     }
 
     property alias slider: control
-    property alias value: control.value
+    property real value
     property alias minimumValue: control.from
     property alias maximumValue: control.to
     property alias stepSize: control.stepSize
     required property /*BrightnessItem.Type*/ int type
+
+    Binding {
+        when: !control.pressed
+        control.value: root.value
+    }
 
     readonly property real percentage: Math.round(100 * value / maximumValue)
     readonly property string brightnessLevelOff: i18nc("Backlight on or off", "Off")
@@ -114,7 +119,10 @@ PlasmaComponents3.ItemDelegate {
                 Accessible.description: brightnessValue.text
                 Accessible.onPressAction: this.moved()
 
-                onMoved: root.moved()
+                onMoved: {
+                    root.value = value
+                    root.moved()
+                }
             }
         }
     }

commit 57f1904599655bb77de7e5089a5894d5930bbaf1

applets/brightness: fix Slider binding issues
94a03a3c04787388026a02ff40afd2b77fc9d0f6 introduced a workaround for
devices that send intermediate updates on brightness changes that
interfere with interactively dragging the slider: it binds the value
only while the control is not pressed. But this causes two issues:
- Keyboard interaction also makes the control pressed, which stops
  the Binding and causes it to restore the pre-binding value, 0, before
  applying the change. So e.g. Arrow Left will always reset the slider
  to 0, rather than one step down, the first time each Plasma run.
- Setting the value delegate's property manually breaks the binding we
  set from outside, so further changes from elsewhere (such as global
  shortcuts) will no longer be reflected in the slider, it will stay at
  whatever value it was set to in the widget. (Except for keyboard
  brightness, which has some extra logic to constantly reestablish the
  binding)

To fix this, we make the binding between the delegate more explicit,
setting it to the current value when pressed and a binding while not.
We also change how we communicate changes to the outside; rather than
having a binding that breaks and has to be established constantly, we
keep the delegate's value property always bound and communicate user
input by passing the value as an argument to the signal that causes the
change to happen anyway. This solves both issues and reduces code
complexity as things are better encapsulated (albeit at the cost of
having a bit of imperative state handling in the Slider)

BUG: 523281

applets/brightness/BrightnessItem.qml
@@ -26,12 +26,7 @@ PlasmaComponents3.ItemDelegate {
     property alias stepSize: control.stepSize
     required property /*BrightnessItem.Type*/ int type
 
-    Binding {
-        when: !control.pressed
-        control.value: root.value
-    }
-
-    readonly property real percentage: Math.round(100 * value / maximumValue)
+    readonly property real percentage: Math.round(100 * control.value / maximumValue)
     readonly property string brightnessLevelOff: i18nc("Backlight on or off", "Off")
     readonly property string brightnessLevelLow: i18nc("Brightness level", "Low")
     readonly property string brightnessLevelMedium: i18nc("Brightness level", "Medium")
@@ -40,19 +35,19 @@ PlasmaComponents3.ItemDelegate {
     readonly property string labelText: {
         if (maximumValue == 1) {
             const levels = [brightnessLevelOff, brightnessLevelOn];
-            return levels[value];
+            return levels[control.value];
         } else if (maximumValue == 2) {
             const levels = [brightnessLevelOff, brightnessLevelLow, brightnessLevelHigh];
-            return levels[value];
+            return levels[control.value];
         } else if (maximumValue == 3) {
             const levels = [brightnessLevelOff, brightnessLevelLow, brightnessLevelMedium, brightnessLevelHigh];
-            return levels[value];
+            return levels[control.value];
         } else {
             return i18nc("Placeholder is brightness percentage", "%1%", percentage);
         }
     }
 
-    signal moved()
+    signal moved(real value)
 
     background.visible: highlighted
     highlighted: activeFocus
@@ -115,14 +110,18 @@ PlasmaComponents3.ItemDelegate {
                 activeFocusOnTab: false
                 from: 0
                 stepSize: 1
+                value: root.value
 
                 Accessible.name: root.type === BrightnessItem.Type.Screen ? i18nc("Placeholder is display name", "Display Brightness - %1", root.text) : root.text
                 Accessible.description: brightnessValue.text
-                Accessible.onPressAction: this.moved()
+                Accessible.onPressAction: this.moved(value)
+
+                // while pressed, don't update in response to outside changes, as it can cause
+                // problems if the backend is reporting intermediate values while changing
+                onPressedChanged: value = pressed ? value : Qt.binding(() => root.value)
 
                 onMoved: {
-                    root.value = value
-                    root.moved()
+                    root.moved(value)
                 }
             }
         }

applets/brightness/PopupDialog.qml
@@ -87,7 +87,7 @@ PlasmaExtras.Representation {
 
                     stepSize: maxBrightness/100
 
-                    onMoved: dialog.screenBrightnessControl.setBrightness(displayName, value)
+                    onMoved: value => dialog.screenBrightnessControl.setBrightness(displayName, value)
                     onActiveFocusChanged: if (activeFocus) scrollView.positionViewAtItem(this)
                 }
 
@@ -140,16 +140,8 @@ PlasmaExtras.Representation {
                 KeyNavigation.backtab: KeyNavigation.up
                 KeyNavigation.tab: KeyNavigation.down
 
-                onMoved: dialog.keyboardBrightnessControl.brightness = value
+                onMoved: value => dialog.keyboardBrightnessControl.brightness = value
                 onActiveFocusChanged: if (activeFocus) scrollView.positionViewAtItem(this)
-
-                // Manually dragging the slider around breaks the binding
-                Connections {
-                    target: dialog.keyboardBrightnessControl
-                    function onBrightnessChanged() {
-                        keyboardBrightnessSlider.value = dialog.keyboardBrightnessControl.brightness;
-                    }
-                }
             }
 
             KeyboardColorItem {